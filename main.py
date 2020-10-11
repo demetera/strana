1ed90b6d04806c842dcbbd2f14a3326fdae4b99e
Demetera <hal4ed@gmail.com>
2020-10-11 21:45:13 +0200

    UPDATE comments and explanations

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from strana import Strana, Article
 
 
-s = Strana(day='2019-12-31', page=3)
-s.get_articles()
-print(s.articles)
+#Getting simple list of articles
 
-s.get_full_articles()
-for art in s.articles:
-    print(art)
+# s = Strana(day='2019-12-31', page=3)
+# s.get_articles()
+# print(s.articles)
 
 
-a_url='https://strana.ua/news/294494-v-ukraine-tolko-kirovohradskaja-oblast-hotova-smjahchit-karantin.html'
-a = Article(a_url)
-print(a.content)
+#Getting detailed list with date
+
+# s.get_full_articles()
+# for art in s.articles:
+#     print(art)
+
+
+#Getting article content in dict
+
+# a_url='https://strana.ua/news/294494-v-ukraine-tolko-kirovohradskaja-oblast-hotova-smjahchit-karantin.html'
+# a = Article(a_url)
+# print(a.content)
