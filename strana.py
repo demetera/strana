9172f8163f2836c7a3bee10a8b6975c1126bfdb6
Demetera <hal4ed@gmail.com>
2020-10-11 21:45:32 +0200

    UPDATE docstrings for classes and functions

--- strana.py
+++ strana.py
@@ -4,8 +4,15 @@ import json
 
 
 class Strana():
+    """Class represents connection to strana.ua"""
     STRANA_URL = 'https://strana.ua'
     def __init__(self, day=None, page=1):
+        """Class initialization
+
+        Args:
+            day (str, optional): date in format yyyy-mm-dd or `yesterday`. Defaults to None.
+            page (int, optional): Page number. Defaults to 1.
+        """
         if day == None:
             self.__requrl = '{}/news/page-{}.html'.format(self.STRANA_URL, page)
         else:
@@ -17,22 +24,41 @@ class Strana():
 
     @property
     def get_title(self):
+        """Returns title of web page
+
+        Returns:
+            str: Title of webpage
+        """
         return self.__title
 
     @property
     def articles(self):
+        """Returns list of articles
+
+        Returns:
+            list: List of tuples (date, text content, article url)
+        """
         return self.__articles
 
     @property
     def request_url(self):
+        """Returns of url compiled when class initialized
+
+        Returns:
+            str: Request URL
+        """
         return self.__requrl
 
     def get_articles(self):
+        """Appends `__articles` with tuple parameters. See `articles` function
+        """
         raw_arts = self.bs.find_all('a', {'class':'article'})
         for art in raw_arts:
             self.__articles.append((art.text.strip(), self.STRANA_URL+art['href']))
 
     def get_full_articles(self):
+        """Appends `__articles` with parameters in tuple. See `articles` function
+        """
         raw_arts = self.bs.find_all('article', {'class':'lenta-news clearfix'})
         for art in raw_arts:
             art_date = art.find('time', {'class': 'date'}).text.replace('\n', '')
@@ -43,6 +69,11 @@ class Strana():
 
 class Article():
     def __init__(self, art_url):
+        """Parses article URL and assignes dict object to `__content` value
+
+        Args:
+            art_url ([type]): [description]
+        """
         conn = requests.get(art_url).text
         parsed = BeautifulSoup(conn, 'html.parser')
         raw_data = parsed.find('script', {'type':'application/ld+json'}).text.strip()
@@ -52,4 +83,9 @@ class Article():
 
     @property
     def content(self):
+        """Returns dict object with article content. See initialization
+
+        Returns:
+            dict: Dict object with article parameters
+        """
         return self.__content
