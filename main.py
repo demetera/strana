60b0d515bad2f4b6429cec3b40a397648853df3c
Demetera <hal4ed@gmail.com>
2020-10-17 22:15:14 +0200

    ADD sql db feature

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from strana import Strana, Article
-
+import sqlite3
 
 #Getting simple list of articles
 
-# s = Strana(day='2019-12-31', page=3)
+s = Strana(day='2018-07-25')
 # s.get_articles()
 # print(s.articles)
 
 
 #Getting detailed list with date
 
-# s.get_full_articles()
-# for art in s.articles:
-#     print(art)
-
+s.get_full_articles()
+db = sqlite3.connect('strana.db')
+q = 'CREATE TABLE IF NOT EXISTS strana (time text, title text, url text)'
+db.execute(q)
+for art in s.articles:
+    q = "INSERT INTO strana VALUES ('{}', '{}' ,'{}')".format(art[0], art[1], art[2])
+    db.execute(q)
+db.commit()
+db.close()
 
 #Getting article content in dict
 
