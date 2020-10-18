3d056dd247c85a0ece581e1c187f921cbd7cb593
Demetera <hal4ed@gmail.com>
2020-10-18 21:26:16 +0200

    CHANGE modified main file

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from strana import Strana, Article
 import sqlite3
 
-#Getting simple list of articles
 
-s = Strana(day='2018-07-25')
-# s.get_articles()
+# s = Strana(query='Запорожье')
+# print(s.request_url)
+# s.get_full_articles()
 # print(s.articles)
 
-
-#Getting detailed list with date
-
-s.get_full_articles()
-db = sqlite3.connect('strana.db')
-q = 'CREATE TABLE IF NOT EXISTS strana (time text, title text, url text)'
-db.execute(q)
-for art in s.articles:
-    q = "INSERT INTO strana VALUES ('{}', '{}' ,'{}')".format(art[0], art[1], art[2])
-    db.execute(q)
-db.commit()
-db.close()
 
 #Getting article content in dict
 
